7a8079658b12e317b08a5e0dfb5b7ff27c551fb5
Marina Petrova <mpetrova@docdoc.ru>
2019-07-07 12:02:33 +0300

    Add test for not present element

--- pages/product_page.py
+++ pages/product_page.py
@@ -1,3 +1,6 @@
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from .base_page import BasePage
 from selenium.webdriver.common.by import By
 from .locators import ProductPageLocators
@@ -5,11 +8,33 @@ class ProductPage(BasePage):
     def add_product_to_basket(self):
         button_basket=self.browser.find_element(*ProductPageLocators.BUTTON_ADD_TO_BASKET)
         button_basket.click()
+
     def check_add_product_message(self):
         product_name=self.browser.find_element(*ProductPageLocators.PRODUCT_NAME)
         product_name_message=self.browser.find_element(*ProductPageLocators.BASKET_MESSAGE)
         assert product_name.text==product_name_message.text
+
     def check_add_product_price(self):
         product_price=self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE)
         product_price_message=self.browser.find_element(*ProductPageLocators.BASKET_PRICE)
         assert product_price.text==product_price_message.text
+
+    def is_not_element_present(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return True
+
+        return False
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE),"Success message is presented, but should not be"
+
+    def is_disappeared(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout, 1, TimeoutException).\
+                until_not(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return False
+
+        return True
--- test_product_page.py
+++ test_product_page.py
@@ -1,7 +1,10 @@
 import pytest
+import time
 from .pages.product_page import ProductPage
+from .pages.locators import ProductPageLocators
 
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0","http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1","http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2","http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3","http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4","http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5","http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6","http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8","http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+@pytest.mark.skip
 def test_guest_can_add_product_to_cart(browser,link):
     #link="http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
     #link="http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
@@ -12,3 +15,27 @@ def test_guest_can_add_product_to_cart(browser,link):
     product_page.solve_quiz_and_get_code() 
     product_page.check_add_product_message() #проверяем название продукта в сообщении
     product_page.check_add_product_price() # проверяем стоимость товаров в корзине
+
+def test_guest_cant_see_success_message_after_adding_product_to_cart(browser):
+    link="http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    browser.get(link)
+    product_page = ProductPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+    product_page.open()
+    product_page.add_product_to_basket() # добавляем товар в корзину
+    product_page.is_not_element_present(*ProductPageLocators.BASKET_MESSAGE) #Проверяем, что нет сообщения об успехе с помощью is_not_element_present
+
+def test_guest_cant_see_success_message(browser):
+    link="http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    browser.get(link)
+    product_page = ProductPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+    product_page.open()
+    product_page.is_not_element_present(*ProductPageLocators.BASKET_MESSAGE) # Проверяем, что нет сообщения об успехе с помощью is_not_element_present
+
+"""def test_message_disappeared_after_adding_product_to_cart(browser):
+    link="http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    browser.get(link)
+    product_page = ProductPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+    product_page.open()
+    product_page.add_product_to_basket()
+    product_page.solve_quiz_and_get_code() 
+    product_page.is_disappeared(*ProductPageLocators.BASKET_MESSAGE) # Проверяем, что нет сообщения об успехе с помощью is_disappeared"""
